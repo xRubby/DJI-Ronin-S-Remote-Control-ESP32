e9e562cbf5dd10565e617ded5e3e59162f110a40
Ruben <rubengigante7@gmail.com>
2025-11-10 15:49:44 +0100

    Aggiunta possibilità di gestire le modalità di movimento (veloce o lenta)

--- Codice/DJI_Ronin_S_RemoteControl_ESP32.ino
+++ Codice/DJI_Ronin_S_RemoteControl_ESP32.ino
@@ -13,6 +13,10 @@ const int sbusMIN = 352;    // Min usable for analog val and switch val
 const int sbusMAX = 1696;   // Max usable for analog val and switch val
 const int sbusWAIT = 10;     // Frame timing delay in msecs
 
+// --- Variabili per la gestione della modalità di movimento (veloce o lenta) ---
+bool lowspeed=false;
+bool prevBState = false;
+
 // --- Callback quando un controller si connette ---
 void onConnectedController(ControllerPtr ctl) {
   if (myController == nullptr) {
@@ -40,13 +44,27 @@ int16_t mapToSbus(int16_t value) {
   }
 
   // Mappatura lineare da -500..500 a sbusMIN..sbusMAX
-  int16_t mapped = map(value, -500, 500, sbusMIN, sbusMAX);
+  int16_t mapped;
+
+  // Se è attiva la modalità lenta, i movimenti verranno eseguiti a velocità ridotta;
+  // altrimenti verranno eseguiti a velocità normale.
+  if(lowspeed){
+    mapped = map(value/2, -500, 500, sbusMIN, sbusMAX);
+  }else{
+    mapped = map(value, -500, 500, sbusMIN, sbusMAX);
+  }
 
   // Assicura che il valore non superi i limiti S.Bus
   if(mapped < sbusMIN) return sbusMIN;
   if(mapped > sbusMAX) return sbusMAX;
 
   return mapped;
+}
+
+void changeSpeed(){
+  lowspeed = !lowspeed;
+
+  Serial.println("Velocità impostata su " + String(lowspeed));
 }
 
 void setup() {
@@ -70,12 +88,20 @@ void loop() {
 
   // Se ci sono dati nuovi e il controller è connesso
   if(dataUpdated && myController != nullptr) {
+    
+    //Se viene premuto il tasto B (A per Switch Pro Controller) la velocità viene dimezzata
+    bool currentBState = myController->b();
+
+    if (currentBState && !prevBState) {
+      changeSpeed();
+    }
+
+    // Aggiorna lo stato precedente del tasto
+    prevBState = currentBState;
 
     // Leggi gli assi del controller e mappa su valori S.Bus
     int16_t pan  = mapToSbus(myController->axisX());
     int16_t tilt = mapToSbus(myController->axisY());
-
-    
 
     // Aggiorna i canali Ronin
     ronin.SetValue(1, pan); // Canale 1 → Pan
